5fc0039a851283b966dbf7025ae3d8a943cbe950
Gilmar Farias Jr <115648013+gilmarfariasjr@users.noreply.github.com>
2023-01-09 09:48:17 -0300

    atualizar

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var velocidade=70
+console.log(`a velocidade do seu carro e ${velocidade}Km/h`)
+if(velocidade > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
